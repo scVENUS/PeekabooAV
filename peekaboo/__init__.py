ea4475f8b1827c0498b0765ed1a8d06ff514f905
Sebastian Deiss <sebastian.deiss@atos.net>
2017-10-17 13:18:20 +0200

    Increment version number and update logo.

--- peekaboo/__init__.py
+++ peekaboo/__init__.py
@@ -27,7 +27,7 @@ import logging
 import re
 
 
-VERSION = (1, 2)
+VERSION = (1, 3)
 AUTHORS = ['Felix Bauer', 'Sebastian Deiss']
 
 __version__ = '.'.join(map(str, VERSION))
@@ -52,11 +52,11 @@ Peekaboo Extended Email Attachment Behavior Observation Owl
                     _aXUXUUU4UUX4XX444UUUUUUXLa,
                    _UXXUXUXU47'!'!'!'!*X444U4UXX,
                    ?XU4U4''   _   ____   -'UUXUUi
-                   ?4U4'     /1| |2222\    'UUXi
-                    *Xi      |1|   __)2|     ?X7
-                     *L      |1|_ /222/      j7
-                      *a     |1(_)22222|    jY
-                       -L,                _/'
+                   ?4U4'     /1| |3333\    'UUXi
+                    *Xi      |1|   __)3|     ?X7
+                     *L      |1|  |333(      j7
+                      *a     |1| _ __)3|    jY
+                       -L,   |1|(_)333/  _/'
                          'l,            _/'
                            j7_a_;  aaa/4
                _aaaaaa#0000#00000##0##00000000aaaaaa,
