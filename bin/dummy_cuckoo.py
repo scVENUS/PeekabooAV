6781c24a1596029f2517ab5bef90a2132c93d1aa
Michael Weiser <michael.weiser@gmx.de>
2020-05-27 14:16:07 +0000

    Drop future use
    Do not use future module and drop dependency: print has always been a
    function and super() is the new-style super() in python 3.6+. Drop
    requirement of the module until a need reemerges.

--- bin/dummy_cuckoo.py
+++ bin/dummy_cuckoo.py
@@ -25,7 +25,6 @@
 ###############################################################################
 
 
-from __future__ import print_function
 from time import sleep
 from sys import stderr
 from datetime import datetime
--- bin/dummy_cuckoo_submit.py
+++ bin/dummy_cuckoo_submit.py
@@ -25,7 +25,6 @@
 ###############################################################################
 
 
-from __future__ import print_function
 from random import choice
 from datetime import datetime
 
